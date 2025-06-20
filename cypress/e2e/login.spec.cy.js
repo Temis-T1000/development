06cee86f7be260c03888c97bdaf299d014223493
Temis Alves <temis.souza@gmail.com>
2025-06-20 11:25:23 -0400

    added userData

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -9,15 +9,22 @@ const selectorsList = {
     wrongCredentialAlert: "[role='alert']"
 }
 
-
-
-
+const userData = {
+    userSucess: {
+      username: 'Admin',
+      password: 'admin123'
+    },
+    userFail: {
+      username: 'teste',
+      password: 'teste'
+    }
+}
 
 describe('Orange HRM Tests', () => {
   it('Login - Sucess', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameField).type('Admin')
-    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.usernameField).type(userData.userSucess.username)
+    cy.get(selectorsList.passwordField).type(userData.userSucess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('eq', '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardLayout)
@@ -25,8 +32,8 @@ describe('Orange HRM Tests', () => {
   })
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameField).type('Admin')
-    cy.get(selectorsList.passwordField).type('test')
+    cy.get(selectorsList.usernameField).type(userData.userFail.username)
+    cy.get(selectorsList.passwordField).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
     cy.get('.oxd-alert-content > .oxd-text')
 
