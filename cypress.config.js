6a39756c1f592dce456550b9095ae4c656ee615d
Temis Alves <temis.souza@gmail.com>
2025-06-21 11:34:50 -0400

    Added baseurl

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      
       // implement node event listeners here
     },
+    baseurl: 'https://opensource-demo.orangehrmlive.com/web/index.php',
   },
 });
