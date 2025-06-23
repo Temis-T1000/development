2a7833f6b704718809f12bdba86b80e32d5be170
Temis Alves <temis.souza@gmail.com>
2025-06-23 12:06:31 -0400

    Added myInfoButton

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -6,18 +6,20 @@ const selectorsList = {
     loginButton: "[type='submit']",
     
     dashboardLayout: ".orangehrm-upgrade-layout",
-    wrongCredentialAlert: "[role='alert']"
+    wrongCredentialAlert: "[role='alert']",
+    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
 }
 
 
 describe('Orange HRM Tests', () => {
-  it('Login - Sucess', () => {
+  it.only('User info upDate - Sucess', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSucess.username)
     cy.get(selectorsList.passwordField).type(userData.userSucess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('eq', '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardLayout)
+    cy.get(selectorsList.myInfoButton).click
     
   })
   it('Login - Fail', () => {
