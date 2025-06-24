b4eb918b9385645145c8d36888074ee150438416
Temis Alves <temis.souza@gmail.com>
2025-06-24 15:04:35 -0400

    User Update scernaio added

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -11,7 +11,9 @@ const selectorsList = {
     firstNameField: "[name='firstName']",
     middleNameField: "[name='middleName']",
     lastNameField: "[name='lastName']",
-    genericField: ".oxd-input"
+    genericField: ".oxd-input",
+    dateField: ".oxd-date-input",
+    buttonSaveSubmit: "[type='submit']",
 }
 
 
@@ -24,15 +26,16 @@ describe('Orange HRM Tests', () => {
     cy.location('pathname').should('eq', '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardLayout)
     cy.get(selectorsList.myInfoButton).click()
-    cy.get(selectorsList.firstNameField).type('Firstname Test')
-    cy.get(selectorsList.middleNameField).type('Middlename Test')
-    cy.get(selectorsList.lastNameField).type('Lastname Test')
+    cy.get(selectorsList.firstNameField).clear().type('Firstname Test')
+    cy.get(selectorsList.middleNameField).clear().type('Middle Test')
+    cy.get(selectorsList.lastNameField).clear().type('Lastname Test')
     cy.get(selectorsList.genericField).eq(4).clear().type('Empl-Test')
     cy.get(selectorsList.genericField).eq(5).clear().type('Other Test')
     cy.get(selectorsList.genericField).eq(6).clear().type('Drivers Number Test')
-    cy.get(selectorsList.genericField).eq(7).clear().type('2035/12/12')
+    cy.get(selectorsList.dateField).eq(0).clear().type('2025-03-10')  
     cy.get(selectorsList.genericField).eq(9).clear().type('TestField  Test')
-    
+    cy.get(selectorsList.buttonSaveSubmit).eq(0).click()
+    cy.get('body').should('contain', 'Successfully Updated')
     
     
     
