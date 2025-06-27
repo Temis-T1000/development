f73ef8a57c99feb01775f07e096527e10db2ce2a
Temis Alves <temis.souza@gmail.com>
2025-06-27 13:18:59 -0400

    Atualization selectors

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,4 +1,7 @@
+
 import LoginPage from '../pages/loginPage.js'
+
+
 import userData from '../fixtures/userData.json'
 
 const loginPage = new LoginPage()
@@ -31,33 +34,31 @@ describe('Orange HRM Tests', () => {
     loginPage.accessLoginPage()
     loginPage.loginWithUser(userData.userSucess.username, userData.userSucess.password)   
     
-    // cy.get(selectorsList.usernameField).type()
-    // cy.get(selectorsList.passwordField).type()
-    // cy.get(selectorsList.loginButton).click()
-    // cy.location('pathname').should('eq', '/web/index.php/dashboard/index')
-    // cy.get(selectorsList.dashboardLayout)
-    // cy.get(selectorsList.myInfoButton).click()
-    // cy.get(selectorsList.firstNameField).clear().type('Firstname Test')
-    // cy.get(selectorsList.middleNameField).clear().type('Middle Test')
-    // cy.get(selectorsList.lastNameField).clear().type('Lastname Test')
-    // cy.get(selectorsList.genericField).eq(4).clear().type('Empl-Test')
-    // cy.get(selectorsList.genericField).eq(5).clear().type('Other Test')
-    // cy.get(selectorsList.genericField).eq(6).clear().type('Drivers Number Test')
-    // cy.get(selectorsList.dateField).eq(0).clear().type('2025-03-10')  
-    // cy.get(selectorsList.genericComboBox).eq(0).click({force: true})
-    // cy.get(selectorsList.secondeItemComboBox).click()
-    // cy.get(selectorsList.genericComboBox).eq(1).click({force: true})
-    // cy.get(selectorsList.thirdiItemComboBox).click()
-    // cy.get(selectorsList.dateBirthday).eq(8).clear().type('1988-28-06')
-    // cy.get('.--close').click()
-    // cy.get(selectorsList.radioOption).eq(1).click()    
-    // cy.get(selectorsList.buttonSaveSubmit).eq(0).click({force: true})
-    // cy.get('body').should('contain', 'Successfully Updated')
+
+    cy.location('pathname').should('eq', '/web/index.php/dashboard/index')
+    cy.get(selectorsList.dashboardLayout)
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstNameField).clear().type('Firstname Test')
+    cy.get(selectorsList.middleNameField).clear().type('Middle Test')
+    cy.get(selectorsList.lastNameField).clear().type('Lastname Test')
+    cy.get(selectorsList.genericField).eq(4).clear().type('Empl-Test')
+    cy.get(selectorsList.genericField).eq(5).clear().type('Other Test')
+    cy.get(selectorsList.genericField).eq(6).clear().type('Drivers Number Test')
+    cy.get(selectorsList.dateField).eq(0).clear().type('2025-03-10')  
+    cy.get(selectorsList.genericComboBox).eq(0).click({force: true})
+    cy.get(selectorsList.secondeItemComboBox).click()
+    cy.get(selectorsList.genericComboBox).eq(1).click({force: true})
+    cy.get(selectorsList.thirdiItemComboBox).click()
+    cy.get(selectorsList.dateBirthday).eq(8).clear().type('1988-28-06')
+    cy.get('.--close').click()
+    cy.get(selectorsList.radioOption).eq(1).click()    
+    cy.get(selectorsList.buttonSaveSubmit).eq(0).click({force: true})
+    cy.get('body').should('contain', 'Successfully Updated')
     
-    // cy.get(selectorsList.genericComboBox).eq(2).click({force: true})
-    // cy.get(selectorsList.bloodItemComboBox).click()
-    // cy.get(selectorsList.genericField).eq(9).clear().type('TestField')
-    // cy.get(selectorsList.buttonSaveSubmit).eq(1).click({force: true})
+    cy.get(selectorsList.genericComboBox).eq(2).click({force: true})
+    cy.get(selectorsList.bloodItemComboBox).click()
+    cy.get(selectorsList.genericField).eq(9).clear().type('TestField')
+    cy.get(selectorsList.buttonSaveSubmit).eq(1).click({force: true})
   })
 
   
--- cypress/pages/loginPage.js
+++ cypress/pages/loginPage.js
@@ -1,3 +1,4 @@
+
 class LoginPage {
     selectorsList() {
         const selectors = {
@@ -12,12 +13,12 @@ class LoginPage {
     accessLoginPage () {
         cy.visit('/auth/login');
     }
+
     loginWithUser(username, password) {
         cy.get(this.selectorsList().usernameField).type(username)
         cy.get(this.selectorsList().passwordField).type(password)
-        cy.get(this.selectorsList().loginButton).click()
-        
+        cy.get(this.selectorsList().loginButton).click()        
     }
 }
 
-export default LoginPage
+    export default LoginPage
